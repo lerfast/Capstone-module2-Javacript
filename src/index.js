f3d36445b40c420d07bf505a994e8db0bcb33107
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-08-01 11:15:14 -0500

    Add new like button
    Adding new like button
    Fix images in each items at main page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import showComments from './modules/popup.js';
+// import likeButtonImage from './assets/heart.svg';
 
 const apiUrl = 'https://api.tvmaze.com/shows';
 
@@ -20,7 +21,14 @@ function renderItems(items) {
   items.forEach((item) => {
     const itemElement = document.createElement('div');
     itemElement.className = 'item'; // Add a class name for styling
-    itemElement.innerHTML = `<h2>${item.name}</h2><p>${item.summary}</p>`;
+    itemElement.innerHTML = `<img class="card__img" src="${item.image.medium}">
+      <h2>${item.name}</h2>
+      <p>${item.summary}</p>
+      <div class="item-buttons">
+        <button class="item-like-btn" data-item-id="${item.id}" aria-label="Like"></button>
+        
+      </div>
+    `;
 
     const commentsButton = document.createElement('button');
     commentsButton.innerText = 'Show Comments'; // Button text
@@ -30,5 +38,17 @@ function renderItems(items) {
     itemsContainer.appendChild(itemElement);
   });
 }
+
+// function handleLikeButtonClick(event) {
+// const itemId = event.target.getAttribute('data-item-id');
+// Add your logic here to handle the like button click for the item with the given itemId
+// For example, you can add the item to a liked list or perform some other action.
+// }
+
+// document.addEventListener('click', (event) => {
+// if (event.target.matches('.item-like-btn')) {
+// handleLikeButtonClick(event);
+// }
+// });
 
 fetchItems().then((items) => renderItems(items));
